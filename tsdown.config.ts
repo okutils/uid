492a05eebdecf520c96d07b0b581dda22b10b81d
Luke Na <narukeu@outlook.com>
2026-03-18 14:15:52 +0800

    feat: 修改构建目标

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
+  entry: ["./src/index.ts"],
+  target: "es2021",
+  format: ["cjs", "esm"],
   dts: {
     tsgo: false,
   },
