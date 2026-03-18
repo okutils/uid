9d667b4cb12019c1980a2cc2d9e5eee2e8868b9f
Luke Na <narukeu@outlook.com>
2026-03-18 13:55:23 +0800

    feat: implement uid function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export function fn() {
-  return "Hello, tsdown!";
-}
+export const uid = (length: number): string => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
